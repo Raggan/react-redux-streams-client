ddcfa0926d436c6f2c197ac994d3d233c88361ed
Simon Holdorf <simon.holdorf@gmail.com>
2019-07-17 16:20:06 +0200

    fixed error handling

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,23 +2,34 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends Component {
+  renderError = ({ error, touched }) => {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  };
   // renderInput(formProps) destructure--> formProps.input formProps.label
-  renderInput({ input, label, meta }) {
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
-        <input {...input} />
-        <div className="">{meta.error}</div>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     );
-  }
+  };
 
   onSubmit(formValues) {}
   render() {
     return (
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form"
+        className="ui form error"
       >
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field
