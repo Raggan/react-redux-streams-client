5e3b0fe638b1f7bd92cc99ed5bc8b17a625362a5
Simon Holdorf <simon.holdorf@gmail.com>
2019-07-17 09:00:00 +0200

    semantic

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
@@ -12,7 +13,10 @@ const App = () => {
       <BrowserRouter>
         <div>
           <Route path="/" exact component={StreamList} />
-          <Route path="/create" component={StreamCreate} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
