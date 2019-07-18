2d18cd51158bcbda9080383a8e0bcd08c57ab3d4
Simon Holdorf <simon.holdorf@gmail.com>
2019-07-18 13:45:41 +0200

    action creator update

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,20 +1,30 @@
 import React from "react";
 import { fetchStream, editStream } from "../../actions";
 import { connect } from "react-redux";
+import StreamForm from "./StreamForm";
 
 class StreamEdit extends React.Component {
   componentDidMount = () => {
     this.props.fetchStream(this.props.match.params.id);
   };
 
+  onSubmit = formValues => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+
   render = () => {
     if (!this.props.stream) {
       return <div>Loading</div>;
     }
+
+    const { title, description } = this.props.stream;
     return (
       <div>
-        <div>{this.props.stream.title}</div>
-        <div>{this.props.stream.description}</div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          onSubmit={this.onSubmit}
+          initialValues={{ title, description }}
+        />
       </div>
     );
   };
