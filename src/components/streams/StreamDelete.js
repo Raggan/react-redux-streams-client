013426964243c0d630ce229abdd88fe4a045491b
Simon Holdorf <simon.holdorf@gmail.com>
2019-07-19 15:30:35 +0200

    modal refactoring

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -2,10 +2,19 @@ import React from "react";
 import Modal from "../Modal";
 
 const StreamDelete = () => {
+  const actions = (
+    <React.Fragment>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  );
   return (
     <div>
-      StreamDelete
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Do you really want to delete stream"
+        actions={actions}
+      />
     </div>
   );
 };
